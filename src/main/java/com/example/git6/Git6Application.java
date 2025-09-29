e90566b812936019ec77cc0e196ab37419f3df76
厉佳铭 <196254331@qq.com>
2025-09-29 15:45:07 +0800

    ljm

---
+++ src/main/java/com/example/git6/Git6Application.java
@@ -0,0 +1,13 @@
+package com.example.git6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Git6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Git6Application.class, args);
+    }
+
+}
